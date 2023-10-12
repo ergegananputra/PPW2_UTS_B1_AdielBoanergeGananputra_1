b9258b637de765476ffe3a2729dcd28810888e71
ergegananputra <ergegananputra@gmail.com>
2023-10-12 12:40:03 +0700

    first commit model controller ready

---
+++ resources/views/layouts/header.blade.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+    <div class="container"
+        style="text-align: center;
+        background: rgb(138, 8, 8);
+        color:white">
+        <h1>PEMAIN</h1>
+        <p>by Adiel Boanerge Gananputra 22/500051/SV/21386</p>
+    </div>
+</body>
+</html>
